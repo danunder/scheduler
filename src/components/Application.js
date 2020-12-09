77d1a77e0d5e317f062fe6812220cc949e6c9534
Daniel Robertson <danieljrobertson@gmail.com>
2020-12-09 13:53:18 -0500

    add mostly-functioning day list

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,9 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const appointments = [
   {
@@ -70,8 +54,16 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [ day, setDay ] = useState("Monday");
-  
+  // const today = Date.getDay(Date.now())
+  const [ days, setDays ] = useState([]);
+  const [day, setDay] = useState("Monday")
+
+  useEffect(() => {
+    axios.get("/api/days")
+    .then(response => setDays(response.data));
+
+  }, [])
+
   return (
     <main className="layout">
       <section className="sidebar">
